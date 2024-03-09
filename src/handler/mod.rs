3877e2214aa238a5ae5aa9106d4d4ae83df92771
Jeremy Chone <jeremy.chone@gmail.com>
2024-03-08 20:42:29 -0800

    . minor rename

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,13 +1,13 @@
 // region:    --- Modules
 
 mod impl_rpc_handlers;
-mod rpc_call_error;
 mod rpc_handler;
+mod rpc_handler_error;
 mod rpc_handler_wrapper;
 
 // -- Flatten
-pub use rpc_call_error::*;
 pub use rpc_handler::*;
+pub use rpc_handler_error::*;
 pub use rpc_handler_wrapper::*;
 
 use futures::Future;
