1009eb8cab3c3414589392c1e76003ba1c533885
Jeremy Chone <jeremy.chone@gmail.com>
2024-03-08 18:56:59 -0800

    ^ params - add blanket IntoRpcParams for Option<D> and Value

--- src/into_rpc_params.rs
+++ src/into_rpc_params.rs
@@ -31,3 +31,31 @@ where
 		}
 	}
 }
+
+// region:    --- Blanket implementation
+
+// IMPORTANT: Probably need to be put below a feature, like `with-blanket-option-params`
+
+/// Implements `IntoRpcParams` for any type that also implements `IntoRpcParams`.
+///
+/// Note: Application code might prefer to avoid this blanket implementation.
+impl<D> IntoRpcParams for Option<D>
+where
+	D: DeserializeOwned + Send,
+	D: IntoRpcParams,
+{
+	fn into_params(value: Option<Value>) -> Result<Self> {
+		let value = value.map(|v| serde_json::from_value(v)).transpose()?;
+		Ok(value)
+	}
+}
+
+// IMPORTANT: Probably need to be put below a feature, like `with-blanket-value-params`
+
+/// This is the IntoRpcParams implementation for serde_json Value.
+///
+/// Note: As above, this might not be a capability app code might want to
+///       allow for rpc_handlers, prefering to have everything strongly type.
+impl IntoRpcParams for Value {}
+
+// endregion: --- Blanket implementation
